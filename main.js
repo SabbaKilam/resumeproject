69135f33253c1d86b14ee99df9343bf2740bf57b
sabbakilam1 <pitdrop@gmail.com>
2016-03-24 02:08:00 +0000

    basic flipping, wrong timing

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 /*
     Author: Abbas Abdulmalik
     Created: March 21, 2016
-    Revised: March 22, 2016    
+    Revised: March 23, 2016
     Title: Resume Project (Abbas Abdulmalik)
     Purpose: An online resume seeking web dev position.
-    Notes: The "look" and underlying code should
-    showcase my actual and latent talents (warts and all).
-    
+			 The "look" and underlying code should
+			 showcase my actual and latent talents
+			 (warts and all).
+	Notes:
+
 //=========| Map of index.html's elements |========
-   
+
 <div id="mobileHolder" class="fullScreen">
     <div id="mainPanel" class="fullScreen">
         <div id="coverPage" class="page">cover page</div>
@@ -16,17 +18,22 @@
         <div id="lowerPage" class="page">lower page</div>
     </div>
     <div id="menu">
-        <div id="menuIcon" >&Congruent;</div> 
-        <div id="menuChoices">menu choices</div>        
+        <div id="menuIcon" >&Congruent;</div>
+        <div id="menuChoices">menu choices</div>
     </div>
-    <div id="splashPanel" class="fullScreen">splash panel</div>        
+    <div id="splashPanel" class="fullScreen">splash panel</div>
 </div>
 */
 //===| START of Application |===
 
 window.onload = function(){
-    
+
 //=========| CONSTANTS, &tc. |=========
+
+	var lorem = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.<br/>" +
+	"Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero," +
+	"sit amet commodo magna eros quis urna.<br/>" +
+	"Nunc viverra imperdiet enim.<br/>Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
 
     var id = function id(ID){return document.getElementById(ID);}
     var mainPanel = id("mainPanel");
@@ -39,17 +46,15 @@ window.onload = function(){
     var menuIcon = id("menuIcon");
     var exitIcon = id("exitIcon");
     var choices = id("menuChoices");
-    
+
     var minFontSize = 10;
-    //var maxFontSize = 18;
 
-    var upperBackground = "linear-gradient(hsla(160, 35%, 100%, 0.8), hsla(160, 35%, 75%, 0.8))"    
-    var lowerBackground = "linear-gradient(hsla(160, 35%, 75%, 0.8), hsla(160, 35%, 50%, 0.8))";
-    
-    flipperBusy = false;
+    var upperBackground = "linear-gradient(hsla(160, 35%, 100%, 1), hsla(160, 35%, 75%, 1)"
+    var lowerBackground = "linear-gradient(hsla(160, 35%, 75%, 1), hsla(160, 35%, 50%, 1))";
 
 
-//=========| STATE VARIABLES |==============
+
+//=========| GLOBAL STATE VARIABLES |==============
 
     var toggleOn = true;
     var menuShowing = false;
@@ -57,12 +62,16 @@ window.onload = function(){
     var totalNumberOfPages = 0;
     var currentPageNumber = 0;
     var currentPage = "";
-    var nextPage = ""
+    var nextPage = "";
+    var flags = {
+		flipperBusy: false,
+		flashingBusy: false
+	};
     var cache = {};
 
 //=========| THE DRIVER'S SEAT |============
-
-    initialize(); 
+	splashPanel.onclick = function(){splashPanel.style.opacity = "0";}
+    initialize();
     coverPage.onclick = flipUp;
     lowerPage.onclick = flipUp;
     upperPage.onclick = flipDown;
@@ -75,25 +84,25 @@ window.onload = function(){
 function initialize(){
     dissolveSplashPanel(); //might place this further down to hide some delays
     adjustRootEm();
-    window.onresize = adjustRootEm;  
+    window.onresize = adjustRootEm;
     supportsLocalStorage = !!(typeof Storage && window.localStorage);
     closeMenuOnPageClick();
     handleMenuSelection();
     mainPanel.onclick = closeMenu;
-    
+
+
     //---| Helper functions |---
     function dissolveSplashPanel(){
         splashPanel.style.opacity = "0";
         splashPanel.style.visibility = "hidden";
-    };    
+
+    };
     function adjustRootEm(){
         var newRootEm = (minFontSize + window.innerWidth/100);
-        document.documentElement.style.fontSize = newRootEm + "px";        
-
-        //temporary test:
-        //coverPage.innerHTML = newRootEm.toFixed(2) + "px";        
+        document.documentElement.style.fontSize = newRootEm + "px";
     }
     //---| END of Helper functions |---
+
 }//===| END of initialize function |=====
 
 function toggle(onHandler, offHandler, e){
@@ -111,26 +120,27 @@ function toggle(onHandler, offHandler, e){
 
 function openMenu(e){
     e.stopPropagation();
-    menuShowing = true;    
+    menuShowing = true;
     menu.style.visibility = "visible";
     menu.style.opacity = "1";
     exitIcon.style.visibility = "visible";
-    exitIcon.style.opacity = "1";    
-    flashMenuIconWhite(0.25);
+    exitIcon.style.opacity = "1";
+    //flashMenuIconWhite(0.25);
+    flashObjectColor(menuIcon, "red", 0.25);
 }
 
 function closeMenu(e){
-    e.stopPropagation();   
+    e.stopPropagation();
     menu.style.visibility = "hidden";
     menu.style.opacity = "0";
     exitIcon.style.visibility = "hidden";
     exitIcon.style.opacity = "0";
-    if(menuShowing){flashMenuIconWhite(0.25);}    
+    if(menuShowing){ flashObjectColor(menuIcon, "red", 0.25);}
     //prevent page flip by delaying state flag changes
     setTimeout(function(){
         toggleOn = true;
         menuShowing = false;
-    },1);
+    },10);
 }
 
 function flashMenuIconWhite(seconds){
@@ -138,99 +148,142 @@ function flashMenuIconWhite(seconds){
 }
 
 function flashObjectColor(object, color, seconds){
+	if(flags.flashingBusy)return;
+	flags.flashingBusy = true;
     seconds = seconds || 0.25;
     var oldColor = object.style.color;
     object.style.color = color;
     setTimeout(function(){
         object.style.color = oldColor;
     }, 1000*seconds);
+	setTimeout(function(){
+		flags.flashingBusy = false;
+	}, 1100);
 }
 
 function flipUp(){
     if(!menuShowing){flipFlipperUp()};
+
 }
 
 function flipDown(){
-    if(!menuShowing){flipFlipperDown()};   
+    if(!menuShowing){flipFlipperDown()};
 }
 
 function pushFlipperDown(){
     flipper.style.bottom = "0";
-    flipper.style.top = "50%";    
+    flipper.style.top = "50%";
+
 }
 function pushFlipperUp(){
     flipper.style.top = "0";
-    flipper.style.bottom = "50%";    
+    flipper.style.bottom = "50%";
+
 }
 function pushFlipperForward(){
     flipper.style.zIndex = "4";
+
 }
 function pushFlipperBack(){
     flipper.style.zIndex = "-1";
 }
 
 function showFlipper(){
-    flipper.style.visibility = "visible";
+	flipper.style.visibility = "visible";
     flipper.style.opacity = "1";
 }
-function hideFlipper(){
-    flipper.style.visibility = "hidden";
-    flipper.style.opacity = "0";    
+
+function hideFlipper(delay){
+	setTimeout(function(){
+		flipper.style.visibility = "hidden";
+	}, delay * 1000);
+    //flipper.style.opacity = "0";
 }
+
 function flipFlipperUp(){
-    if(flipperBusy)return;
-    flipperBusy = true;
+    if(flags.flipperBusy)return;
+    flags.flipperBusy = true;
     flipper.style.background = lowerBackground;
-    
-    flipper.style.borderTopLeftRadius = "none";
-    flipper.style.borderTopRightRadius = "none";
-    flipper.style.borderBottomLeftRadius = "0.5rem";
-    flipper.style.borderBottomRightRadius = "0.5rem";
-    
-    flipper.style.borderTop = "1px solid hsla(160, 40%, 60%,1)"; 
-    pushFlipperDown();    
-    pushFlipperForward();
-    showFlipper();
+
+    //make only lower corners round
+	flipper.style.borderTopLeftRadius = "0";
+    flipper.style.borderTopRightRadius = "0";
+    flipper.style.borderBottomLeftRadius = "1rem";
+    flipper.style.borderBottomRightRadius = "1rem";
+	flipper.style.border = "1px solid lightgray";
+	flipper.style.borderTop = "none";
+
+	//put flipper into position before showing the flip
+	pushFlipperDown();
     flipper.style.transformOrigin = "top";
+    pushFlipperForward();
+
+    //handle content
+    handleContent(lowerPage);
+
+    //Here's the up flip!
+	showFlipper();
     flipper.style.transform = "rotateX(180deg)";
-    setTimeout(function(){
-        flipper.style.background = upperBackground;        
-        hideFlipper();
-        flipper.style.transform = "rotateX(0deg)";        
+
+    //Let the flipped page show for a bit before hiding
+	setTimeout(function(){
+        hideFlipper(0.5);
         pushFlipperBack();
-        pushFlipperDown();        
-        flipper.style.borderTop = "none";
+        pushFlipperDown();
         flipper.style.background = lowerBackground;
-        flipperBusy = false;    
-    },1300)
+    },500);
+	//Let prior preperation happen before next flip allowed
+	setTimeout(function(){
+        flipper.style.transform = "rotateX(0deg)";
+        flags.flipperBusy = false;
+	},2200);
 }
 
 function flipFlipperDown(){
-    if(flipperBusy)return;
-    flipperBusy = true;
+    if(flags.flipperBusy)return;
+    flags.flipperBusy = true;
     flipper.style.background = upperBackground;
-    
-    flipper.style.borderTopLeftRadius = "0.5rem";
-    flipper.style.borderTopRightRadius = "0.5rem";
-    flipper.style.borderBottomLeftRadius = "none";
-    flipper.style.borderBottomRightRadius = "none";
-    
-    flipper.style.borderBottom = "1px solid hsla(160, 40%, 60%,1)"; 
-    pushFlipperUp();    
-    pushFlipperForward();
-    showFlipper();
+
+	//make only upper corners round
+    flipper.style.borderTopLeftRadius = "1rem";
+    flipper.style.borderTopRightRadius = "1rem";
+    flipper.style.borderBottomLeftRadius = "0";
+    flipper.style.borderBottomRightRadius = "0";
+	flipper.style.border = "1px solid gray;"
+	flipper.style.borderBottom = "none";
+
+	//put flipper into position before showing the flip
+    pushFlipperUp();
     flipper.style.transformOrigin = "bottom";
+    pushFlipperForward();
+
+    //handle content
+    handleContent(upperPage);
+
+    //Here's the down flip!
+	showFlipper();
     flipper.style.transform = "rotateX(-180deg)";
+
+    //Let the flipped page show for a bit before hiding
     setTimeout(function(){
-        flipper.style.background = lowerBackground;        
-        hideFlipper();
-        flipper.style.transform = "rotateX(0deg)";        
+        hideFlipper(0.5);
+        //flipper.style.transform = "rotateX(0deg)";
         pushFlipperBack();
-        pushFlipperUp();        
-        flipper.style.borderTop = "none";
+        pushFlipperUp();
         flipper.style.background = upperBackground;
-        flipperBusy = false;    
-    },1300)
+    },500);
+	//Let prior preperation happen before next flip allowed
+	setTimeout(function(){
+        flipper.style.transform = "rotateX(0deg)";
+        flags.flipperBusy = false;
+	},2200);
+}
+
+function handleContent(page){
+	flipper.innerHTML = page.innerHTML;
+	setTimeout(function(){
+		page.innerHTML = lorem;
+	},200);
 }
 
 function loadChoice(){}
@@ -241,7 +294,7 @@ function closeMenuOnPageClick(){
         m.addEventListener("click",function(e){
             closeMenu(e);
         });
-    });    
+    });
 }
 
 function getClassArray(className){
@@ -258,10 +311,16 @@ function getClassArray(className){
 function handleMenuSelection(){
     var menuChoices = getClassArray("li");
     menuChoices.forEach(function(m){
-        m.addEventListener("click", function(e){
+        m.addEventListener("click", function(e,m){
            //handle request here
-           flashObjectColor(e.target, "white", 0.5);
-        }); 
+           flashObjectColor(e.target, "red", 0.5);
+           setTimeout(function(){
+				closeMenu(e);
+				var fixedText = e.target.innerHTML.split(" ").join("").toLowerCase();
+				upperPage.innerHTML += fixedText +"<br/>";
+				lowerPage.innerHTML += fixedText +"<br/>";
+           }, 500);
+        });
     });
 }
 
